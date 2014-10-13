b8acb43fb75aba9633f1940eaff252d852196477
KangRoger <kangroger@qq.com>
2014-10-13 20:39:45 +0800

    对一个互斥量两次加锁导致死锁

---
+++ DeadLocktest2.cpp
@@ -0,0 +1,33 @@
+﻿#include<thread>
+#include<mutex>
+#include<iostream>
+class Test
+{
+private:
+	std::mutex m1;
+public:
+	void fun1()
+	{
+		std::lock_guard<std::mutex> guard1(m1);
+		fun2();
+		
+	}
+	void fun2()
+	{
+		std::lock_guard<std::mutex> guard1(m1);
+	}
+
+};
+void fun(Test *p)
+{
+	p->fun1();
+	std::cout << "fun1" << std::endl;
+}
+
+int main()
+{
+	Test t;
+	std::thread A(fun, &t);
+	A.join();
+	return 0;
+}
