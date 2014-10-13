ddb69e2a934f01ac6fd0c0f717f45ac017893090
KangRoger <kangroger@qq.com>
2014-10-13 20:39:26 +0800

    加锁顺序不同导致死锁

---
+++ DeadLocktest1.cpp
@@ -0,0 +1,42 @@
+﻿#include<thread>
+#include<mutex>
+#include<unistd.h>
+class Test
+{
+private:
+	std::mutex m1;
+	std::mutex m2;
+public:
+	void fun1()
+	{
+		std::lock_guard<std::mutex> guard1(m1);
+		//休眠,使死锁更容易发生
+		sleep(1);
+		std::lock_guard<std::mutex> guard2(m2);
+	}
+	void fun2()
+	{
+		std::lock_guard<std::mutex> guard1(m2);
+		//休眠,使死锁更容易发生
+		sleep(1);
+		std::lock_guard<std::mutex> guard2(m1);
+	}
+
+};
+void fun1(Test *p)
+{
+	p->fun1();
+}
+void fun2(Test *p)
+{
+	p->fun2();
+}
+int main()
+{
+	Test t;
+	std::thread A(fun1, &t);
+	std::thread B(fun2, &t);
+	A.join();
+	B.join();
+	return 0;
+}
